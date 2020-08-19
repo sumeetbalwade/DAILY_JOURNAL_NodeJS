1f5911f8346af3768213026582718534307d0508
Sumeet Balwade <balwadesr@gmail.com>
2020-08-19 13:30:26 +0530

    done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const port = process.env.port || 3000;
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Welcome to the anonymous Blog Site write the blogs you need.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -40,19 +40,21 @@ const defaultItem = new Post({
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 });
 
+let check = 0;
 app.get("/", function (req, res) {
   Post.find({}, (err, findedList) => {
     if (!err) {
       if (findedList.length === 0) {
         let posts = [defaultItem];
-        Post.insertMany(posts, (err) => {});
-        res.redirect("/");
-      } else if (findedList.length > 1) {
-        Post.findOneAndDelete(
-          { postTitle: "Example Post" },
-          (err, result) => {}
-        );
-        res.redirect("/");
+        Post.insertMany(posts, (err) => {
+          check = 1;
+          res.redirect("/");
+        });
+      } else if (findedList.length > 1 && check === 1) {
+        Post.findOneAndDelete({ postTitle: "Example Post" }, (err, result) => {
+          check = 0;
+          res.redirect("/");
+        });
       } else {
         res.render("home", {
           startingContent: homeStartingContent,
