ef710849b411295d1366e489be57091fc7f6f200
Sumeet Balwade <balwadesr@gmail.com>
2020-08-19 13:50:52 +0530

    config heroku

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.get("/posts/:postId", function (req, res) {
   });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server started on port " + port);
 });
